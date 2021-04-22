ebf0b9d27c7f6a1f8249f5bb78333d92250c41ac
Ekaterina <ekaterina.latysheva01@gmail.com>
2021-04-22 17:20:08 +0300

    тест для класса Point

---
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -0,0 +1,15 @@
+package ru.stqa.pft.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PointTests {
+@Test
+  public void testPoint() {
+
+    Point p1 = new Point (1,2);
+    Point p2 = new Point (5,5);
+  Assert.assertEquals (p1.distance(p2) , 5);
+  }
+
+}
