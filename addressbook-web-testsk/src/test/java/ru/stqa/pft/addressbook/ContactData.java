6857b1e8873e65fa2df8101149ff6750df8a13ff
Ekaterina <ekaterina.latysheva01@gmail.com>
2021-05-04 16:02:25 +0300

    тесты для создания групп и контактов в адресной книге

---
+++ addressbook-web-testsk/src/test/java/ru/stqa/pft/addressbook/ContactData.java
@@ -0,0 +1,31 @@
+package ru.stqa.pft.addressbook;
+
+public class ContactData {
+  private final String firstname;
+  private final String lastname;
+  private final String home;
+  private final String mail;
+
+  public ContactData(String firstname, String lastname, String home, String mail) {
+    this.firstname = firstname;
+    this.lastname = lastname;
+    this.home = home;
+    this.mail = mail;
+  }
+
+  public String getFirstname() {
+    return firstname;
+  }
+
+  public String getLastname() {
+    return lastname;
+  }
+
+  public String getHome() {
+    return home;
+  }
+
+  public String getMail() {
+    return mail;
+  }
+}
