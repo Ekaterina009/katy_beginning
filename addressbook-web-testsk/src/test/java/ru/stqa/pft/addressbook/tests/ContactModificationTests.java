afad1316f6365890dbd5626edc3f723c872fb429
Ekaterina <ekaterina.latysheva01@gmail.com>
2021-05-06 17:01:59 +0300

    тесты для  модификации групп и контактов

---
+++ addressbook-web-testsk/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -0,0 +1,16 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.ContactData;
+
+public class ContactModificationTests extends TestBase {
+
+  @Test
+
+  public void testContactModification() {
+    app.getContactHelper().editContact();
+    app.getContactHelper().fillContactForm(new ContactData("Harryup", "Potterup", "+8944885991", "HarryPotter@wizard.com"));
+    app.getContactHelper().submitContactModification();
+
+  }
+}
