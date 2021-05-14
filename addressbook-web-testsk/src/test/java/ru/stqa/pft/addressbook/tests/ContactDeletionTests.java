46eddd2a9462397c0b5417dc1a8ee1c42cbcbca2
Ekaterina <ekaterina.latysheva01@gmail.com>
2021-05-14 15:14:28 +0300

    4.6. Сравнение коллекций

--- addressbook-web-testsk/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-testsk/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
@@ -20,6 +20,9 @@ public class ContactDeletionTests extends TestBase {
     List<ContactData> after = app.getContactHelper().getContactList();
     Assert.assertEquals(after.size(), before.size() - 1);
     app.logout();
+
+    before.remove(before.size() - 1);
+    Assert.assertEquals(before, after);
   }
 
 }
