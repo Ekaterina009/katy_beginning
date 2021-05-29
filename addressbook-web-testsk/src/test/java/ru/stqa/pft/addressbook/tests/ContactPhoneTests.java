65bc4d527107f76b71bb355292d4ee7655b5b8b3
Ekaterina <ekaterina.latysheva01@gmail.com>
2021-05-29 22:05:03 +0300

    5.10. Режем строки

--- addressbook-web-testsk/src/test/java/ru/stqa/pft/addressbook/tests/ContactPhoneTests.java
+++ addressbook-web-testsk/src/test/java/ru/stqa/pft/addressbook/tests/ContactPhoneTests.java
@@ -3,12 +3,23 @@ package ru.stqa.pft.addressbook.tests;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 public class ContactPhoneTests extends TestBase {
 
   @Test
   public void testContactPhones() {
     ContactData contact = app.contact().all().iterator().next();
     ContactData contactInfoFromEditForm = app.contact().infoFromEditForm(contact);
+
+    assertThat(contact.getHome(), equalTo(cleaned(contactInfoFromEditForm.getHome())));
+    assertThat(contact.getMobilePhone(), equalTo(cleaned(contactInfoFromEditForm.getMobilePhone())));
+    assertThat(contact.getWorkPhone(), equalTo(cleaned(contactInfoFromEditForm.getWorkPhone())));
+  }
+
+  public String cleaned (String phone) {
+    return phone.replaceAll("\\s", "").replaceAll("[-()]", "");
   }
 
 }
