3b058c8037dbe147decbc7cd4e900146bd923d3a
Ekaterina <ekaterina.latysheva01@gmail.com>
2021-06-01 21:42:17 +0300

    6.5. Загрузка тестовых данных из файла

--- addressbook-web-testsk/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-testsk/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -5,7 +5,7 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 
-import java.io.File;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -17,14 +17,15 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class ContactCreationTests extends TestBase {
 
   @DataProvider
-  public Iterator<Object[]> validContacts() {
+  public Iterator<Object[]> validContacts() throws IOException {
     List<Object[]> list = new ArrayList<Object[]>();
-    list.add(new Object[] {new ContactData().withFirstname("Harry").withLastname("Potter").withHome("+8944885995")
-            .withMail("HarryPotter@wizard.com")});
-    list.add(new Object[] {new ContactData().withFirstname("Bill").withLastname("Kill").withHome("+8944885996")
-            .withMail("BillKill@unit.com")});
-    list.add(new Object[] {new ContactData().withFirstname("Marry").withLastname("Key").withHome("+8944885997")
-            .withMail("MarryKey@corporation.com")});
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.csv")));
+    String line = reader.readLine();
+    while (line != null) {
+      String[] split = line.split(";");
+      list.add(new Object[] {new ContactData().withFirstname(split[0]).withLastname(split[1])});
+      line = reader.readLine();
+    }
     return list.iterator();
   }
 
