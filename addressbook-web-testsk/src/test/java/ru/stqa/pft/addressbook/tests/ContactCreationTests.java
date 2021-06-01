3c9feb0b3f5072edadfa911dccf87f909631d872
Ekaterina <ekaterina.latysheva01@gmail.com>
2021-06-01 21:02:32 +0300

    6.4. Параметризация тестовых методов

--- addressbook-web-testsk/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-testsk/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,10 +1,14 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -12,19 +16,28 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBase {
 
-
-  @Test
-  public void testContactCreationTests() throws Exception {
-    Contacts before = app.contact().all();
-    File photo = new File("src/test/resources/2021-05-31.png");
-    ContactData contact = new ContactData().withFirstname("Harry").withLastname("Potter")
-            .withHome("+8944885995").withMail("HarryPotter@wizard.com").withPhoto(photo);
-    app.contact().create(contact);
-    assertThat(app.contact().count(), equalTo(before.size() + 1));
-    Contacts after = app.contact().all();
-   // app.logout();
-    assertThat(after, equalTo(
-            before.withAdded(contact.withId(after.stream().mapToInt((c) ->c.getId()).max().getAsInt()))));
+  @DataProvider
+  public Iterator<Object[]> validContacts() {
+    List<Object[]> list = new ArrayList<Object[]>();
+    list.add(new Object[] {new ContactData().withFirstname("Harry").withLastname("Potter").withHome("+8944885995")
+            .withMail("HarryPotter@wizard.com")});
+    list.add(new Object[] {new ContactData().withFirstname("Bill").withLastname("Kill").withHome("+8944885996")
+            .withMail("BillKill@unit.com")});
+    list.add(new Object[] {new ContactData().withFirstname("Marry").withLastname("Key").withHome("+8944885997")
+            .withMail("MarryKey@corporation.com")});
+    return list.iterator();
   }
 
+
+  @Test(dataProvider = "validContacts")
+  public void testContactCreationTests(ContactData contact) throws Exception {
+      Contacts before = app.contact().all();
+      File photo = new File("src/test/resources/2021-05-31.png");
+      app.contact().create(contact);
+      assertThat(app.contact().count(), equalTo(before.size() + 1));
+      Contacts after = app.contact().all();
+      // app.logout();
+      assertThat(after, equalTo(
+              before.withAdded(contact.withId(after.stream().mapToInt((c) ->c.getId()).max().getAsInt()))));
+    }
 }
